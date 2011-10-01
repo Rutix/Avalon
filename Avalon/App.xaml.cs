181717ddd777bb46803e62a2599f3ca6ae67c880
Rutix <rutgerstorm@gmail.com>
2011-10-01 23:45:47 +0200

    Added the module interface and application startup code

--- Avalon/App.xaml.cs
+++ Avalon/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -12,5 +13,20 @@ namespace Avalon
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// The startup of the application.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.Windows.StartupEventArgs"/> instance containing the event data.</param>
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+
+            var catalog = new AssemblyCatalog(this.GetType().Assembly);
+            var container = new CompositionContainer(catalog);
+            var modules = container.GetExportedValues<IModule>();
+
+            mainWindow.Show();
+        }
     }
 }
---
+++ Avalon/IModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Avalon
+{
+    /// <summary>
+    /// The interface for modules.
+    /// </summary>
+    interface IModule
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets the user interface.
+        /// </summary>
+        UserControl UserInterface { get; }
+    }
+}
