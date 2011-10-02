7e9049d61144b23366d37ef02be5b61e04d74622
Rutix <rutgerstorm@gmail.com>
2011-10-02 21:57:34 +0200

    Added a test module

---
+++ Module1/Module1.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using Avalon;
+using ModuleTracking;
+
+namespace Module1
+{
+    [Export(typeof(IModule))]
+    public class Module1 : IModule
+    {
+        public string Name
+        {
+            get { return "module 1"; }
+        }
+
+        public UserControl UserInterface
+        {
+            get { return new Module1View(); }
+        }
+    }
+}
