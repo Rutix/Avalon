96d93fa5868b1b5ff51eb7400ba8089778ca328c
Rutix <rutgerstorm@gmail.com>
2011-10-10 21:44:24 +0200

    Replaced Module1 with PortScan.

---
+++ Avalon.PortScan/PortScan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using ModuleTracking;
+
+namespace AvalonPortScan
+{
+    [Export(typeof(IModule))]
+    class PortScan : IModule
+    {
+        public string Name
+        {
+            get { return "Port Scanner"; }
+        }
+
+        public UserControl UserInterface
+        {
+            get { return new PortScanView(); }
+        }
+    }
+}
