6dac0dfab02141531c33d1f021fa3f6f6122969d
Rutix <rutgerstorm@gmail.com>
2011-10-16 22:14:39 +0200

    Added IPAddressResolver class to the PortScan Module

---
+++ Avalon.PortScan/IPAddressResolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace AvalonPortScan
+{
+    class IPAddressResolver
+    {
+        /// <summary>
+        /// Determines whether address is an IPAddress.
+        /// </summary>
+        /// <param name="address">The address.</param>
+        /// <returns>
+        ///   <c>true</c> if address is an IP address ; otherwise, <c>false</c>.
+        /// </returns>
+        static bool IsAnIPAddress(string address)
+        {
+            IPAddress ipAddress;
+            return IPAddress.TryParse(address, out ipAddress);
+        }
+
+        /// <summary>
+        /// Gets the IP address from DNS.
+        /// </summary>
+        /// <param name="scanAddress">The scan address.</param>
+        /// <returns></returns>
+        static List<IPAddress> GetIPAddressFromDNS(string scanAddress)
+        {
+            var ips = Dns.GetHostAddresses(scanAddress).ToList();
+            if (ips.Count() > 0)
+            {
+                return ips;
+            }
+            throw new ArgumentException(string.Format("Unable to lookup hot name '{0}'.", scanAddress));
+        }
+
+        /// <summary>
+        /// Resolves the IP address.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <returns></returns>
+        public static List<IPAddress> ResolveIPAddress(string target)
+        {
+            if (IsAnIPAddress(target))
+            {
+                var ipaddress = new List<IPAddress>();
+                ipaddress.Add(IPAddress.Parse(target));
+                return ipaddress;
+            }
+
+            try
+            {
+                return GetIPAddressFromDNS(target);
+            }
+            catch (ArgumentException)
+            {
+                
+            }
+            return null;
+        }
+    }
+}
