400825bf9990ba0dd576611140932c24e89c9389
Rutix <rutgerstorm@gmail.com>
2011-10-16 22:17:53 +0200

    Initial commit for PortScanViewModel

---
+++ Avalon.PortScan/PortScanViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AvalonPortScan
+{
+    class PortScanViewModel
+    {
+        private static IEnumerable Model;
+
+        public static void Test()
+        {
+            foreach (var test in Model)
+            {
+                test.ToString();
+            }
+        }
+    }
+}
