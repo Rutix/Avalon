70c1e35f596740728cb64bc6c612efd09ac7abcc
Rutix <rutgerstorm@gmail.com>
2011-10-23 01:06:50 +0200

    Added an Unittest project, MainWindowViewModelTests and ViewModelBaseTest

---
+++ Avalon.Tests/Avalon.Common/ViewModelBaseTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Avalon.Common;
+using NUnit.Framework;
+
+namespace Avalon.Tests
+{
+    [TestFixture]
+    class ViewModelBaseTest
+    {
+        [Test]
+        public void TestPropertyChangedIsRaisedCorrectly()
+        {
+            TestViewModel target = new TestViewModel();
+            bool eventWasRaised = false;
+            target.PropertyChanged += (sender, e) => eventWasRaised = e.PropertyName == "GoodProperty";
+            target.GoodProperty = "Some new value";
+            Assert.IsTrue(eventWasRaised, "PropertyChanged event was not raised correctly.");
+        }
+
+        [Test]
+        public void TestExceptionIsThrownOnInvalidPropertyName()
+        {
+            TestViewModel target = new TestViewModel();
+            try
+            {
+                target.BadProperty = "Some new value...";
+#if DEBUG
+                Assert.Fail("Exception was not thrown when invalid property name was used in DEBUG build.");
+#endif
+            }
+            catch (Exception)
+            {
+#if !DEBUG
+                Assert.Fail("Exception was thrown when invalid property name was used in RELEASE build.");
+#endif
+            }
+        }
+
+        private class TestViewModel : ViewModelBase
+        {
+            protected override bool ThrowOnInvalidPropertyName
+            {
+                get { return false; }
+            }
+
+            public string GoodProperty
+            {
+                get { return null; }
+                set
+                {
+                    base.OnPropertyChanged("GoodProperty");
+                }
+            }
+
+            public string BadProperty
+            {
+                get { return null; }
+                set
+                {
+                    base.OnPropertyChanged("ThisIsAnInvalidPropertyName!");
+                }
+            }
+        }
+    }
+}
---
+++ Avalon.Tests/Avalon/MainWindowViewModelTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using Avalon.Common;
+using Moq;
+using NUnit.Framework;
+
+namespace Avalon.Tests
+{
+    [TestFixture]
+    class MainWindowViewModelTests
+    {
+
+        [Test]
+        public void FirstModuleShouldBeSelectedByDefault()
+        {
+            IModule module = new Mock<IModule>().Object;
+            MainWindowViewModel vm = new MainWindowViewModel(new IModule[] {module});
+            Assert.AreSame(vm.SelectedModule, module);
+        }
+
+        [Test]
+        [RequiresSTA]
+        public void ShowsUserInterfaceOfSelectedModule()
+        {
+            var mock = new Mock<IModule>();
+            var ui = new Mock<UserControl>();
+            mock.Setup(m => m.UserInterface).Returns(ui.Object);
+            MainWindowViewModel vm = new MainWindowViewModel(new IModule[] { mock.Object });
+            Assert.AreSame(ui.Object, vm.UserInterface);
+        }
+
+        [Test]
+        public void IfNoModulesUserInterfaceReturnsNull()
+        {
+            MainWindowViewModel vm = new MainWindowViewModel(new IModule[] {});
+            Assert.IsNull(vm.UserInterface);
+        }
+
+        [Test]
+        public void WhenSelectedModuleChangesPropertyChangedEventFires()
+        {
+            var module = new Mock<IModule>().Object;
+            var module2 = new Mock<IModule>().Object;
+            MainWindowViewModel vm = new MainWindowViewModel(new IModule[] {module, module2});
+            List<string> propertiesChanged = new List<string>();
+            vm.PropertyChanged += (sender, args) => propertiesChanged.Add(args.PropertyName);
+
+            vm.SelectedModule = vm.Modules[1];
+
+            Assert.AreEqual(2, propertiesChanged.Count);
+            Assert.IsTrue(propertiesChanged.Contains("SelectedModule"));
+            Assert.IsTrue(propertiesChanged.Contains("UserInterface"));
+        }
+    }
+}
