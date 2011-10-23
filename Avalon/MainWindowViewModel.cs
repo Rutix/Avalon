1cba187bfb87242b5700bd862174eface91e77de
Rutix <rutgerstorm@gmail.com>
2011-10-23 16:43:41 +0200

    Added the localized strings and use it to set DisplayName

--- Avalon/MainWindowViewModel.cs
+++ Avalon/MainWindowViewModel.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Windows.Controls;
 using Avalon.Common;
+using Avalon.Properties;
 
 namespace Avalon
 {
+    /// <summary>
+    /// The application's main window ViewModel.
+    /// </summary>
     public class MainWindowViewModel : ViewModelBase
     {
-        #region Variables
+        #region Fields
 
-        private IModule _selectedModule; 
-        
+        private IModule _selectedModule;
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
+        /// <param name="modules">The modules.</param>
+        public MainWindowViewModel(IEnumerable<IModule> modules)
+        {
+            base.DisplayName = Resources.MainWindowViewModel_DisplayName;
+
+            Modules = modules.OrderBy(m => m.Name).ToList();
+            if (Modules.Count > 0)
+            {
+                SelectedModule = Modules[0];
+            }
+
+        }
         #endregion
 
         #region Properties
@@ -50,22 +71,6 @@ namespace Avalon
             get { return SelectedModule == null ? null : SelectedModule.UserInterface; }
         }
         
-        #endregion
-
-        #region Methods
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
-        /// </summary>
-        /// <param name="modules">The modules.</param>
-        public MainWindowViewModel(IEnumerable<IModule> modules)
-        {
-            Modules = modules.OrderBy(m => m.Name).ToList();
-            if (Modules.Count > 0)
-            {
-                SelectedModule = Modules[0];
-            }
-
-        }
         #endregion
     }
 }
