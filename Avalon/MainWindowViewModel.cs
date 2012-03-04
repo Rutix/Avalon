fe1cd88b9361b727019f274dfb269d08ca834510
Rutix <rutgerstorm@gmail.com>
2012-03-04 12:41:30 +0100

    Removed the magic strings and changed them to PropertyOf

--- Avalon/MainWindowViewModel.cs
+++ Avalon/MainWindowViewModel.cs
@@ -57,8 +57,8 @@ namespace Avalon
                 if (value != _selectedModule)
                 {
                     _selectedModule = value;
-                    OnPropertyChanged("SelectedModule");
-                    OnPropertyChanged("UserInterface");
+                    OnPropertyChanged(PropertyOf<MainWindowViewModel>.Resolve(x => x.SelectedModule));
+                    OnPropertyChanged(PropertyOf<MainWindowViewModel>.Resolve(x => x.UserInterface));
                 }
                 _selectedModule = value;
             }
